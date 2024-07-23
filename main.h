277e1c917da6b7a3ba28f4655ad41e5e41f4799b
Bomays <9314@holbertonstudents.com>
2024-07-23 11:07:55 +0200

    adding main.h uploaded & _putchar function file & main.c of the tasks

--- main.h
+++ main.h
@@ -4,19 +4,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
-
+#include <string.h>
 
 typedef struct printer
 {
 	char format;
 	void (*f)(va_list);
-}
+}type_t
 
 
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-//int char_formats(const char *format, va_list args);
+int print_typeformats(const char spec, va_list args);
 
 int _char(va_list ch);
 int _string(va_list str);
