afd9ca9e03258b476a434b243b551841bc7bd93d
GenXwater <louis.genty@outlook.com>
2024-07-24 08:52:26 +0200

    beggin to code

--- _type_formats.c
+++ _type_formats.c
@@ -5,16 +5,28 @@
  * aux fonctions de gestion correspondantes
  */
 
-int print_typeformats(const char spec, va_list ap)
+int print_typeformats(const char spec, va_list args)
 
 
 {
 type_t type_spec[] = {
 	{'c', print_char},
 	{'s', print_string},
+	{'%', print_percent};
 	{NULL, NULL}
 };
 
 int i = 0;
 
-while (type_spec[i].format
+while (type_spec[i].format)
+{
+	if (type_spec[i].format == spec)
+	{
+		type_spec[i].f(args);
+		return (0);
+	}
+	i++;
+
+	/** test */
+	return (1);
+}
