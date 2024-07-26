692440641443cb71d7ad4937eeba53b1c66bd29f
Bomays <9314@holbertonstudents.com>
2024-07-26 10:45:06 +0200

    updated betty code style and doc for print_char file

--- print_char.c
+++ print_char.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
 /**
+ * print_char - va_list linked function that prints a character
  *
+ * @ch: char type, argument inputed in the function
+ * representing the character to print in va_list
  *
+ * Return: return 1 when success
  */
 
 int print_char(va_list ch)
 {
 	int c = va_arg(ch, int);
+
 	_putchar (c);
 	return (1);
 }
